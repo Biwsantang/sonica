2b4dabeb335c5aaf94132915fbfff0db3a8d9717
Biwsantang <biw.santang@icloud.com>
2021-09-04 22:29:23 +0700

    show connection ip address

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,17 @@ var server_cube = [0,0,0];
 
 function makeConnection(info) {
 
-	var connection = new WebSocket('ws://'+info.address+':'+info.port)
+	var ws = new WebSocket('ws://'+info.address+':'+info.port)
 
-	connection.onopen = function () {
+	ws.onopen = function () {
 	  // จะทำงานเมื่อเชื่อมต่อสำเร็จ
 	  console.log("connect webSocket");
-	  connection.send("Hello ABCDEF"); // ส่ง Data ไปที่ Server
-	};
-	connection.onerror = function (error) {
+	  ws.send("Open Connection"); // ส่ง Data ไปที่ Server
+	}
+	ws.onerror = function (error) {
 	  console.error('WebSocket Error ' + error);
 	};
-	connection.onmessage = function (e) {
+	ws.onmessage = function (e) {
 	  // log ค่าที่ถูกส่งมาจาก server
 	  console.log("message from server: ", e.data);
 	  server_cube = JSON.parse(e.data)['ultra'];
--- server.js
+++ server.js
@@ -13,14 +13,15 @@ const wss = new WebSocket.Server({
 
 var read_data = []
 // สร้าง websockets server ที่ port 4000
-wss.on('connection', function connection(ws) { // สร้าง connection
+wss.on('connection', function connection(ws, req) { // สร้าง connection
+    const ip = req.socket.remoteAddress;
     ws.on('message', function incoming(message) {
         // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
-        console.log('received: %s', message);
+        console.log('received from %s - %s', ip, message);
     });
     ws.on('close', function close() {
         // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
-        console.log('disconnected');
+        console.log('%s disconnected', ip);
     });
     //ws.send('init message to client');
     // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้

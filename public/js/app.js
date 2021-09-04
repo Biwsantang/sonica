1afb54aa744653dbd8b62c679db3ab9bf02e51fd
Biwsantang <biw.santang@icloud.com>
2021-09-04 21:49:42 +0700

    fetch ws_info for address and ip of ws server

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,25 @@
 // ทำการเชื่อม Websocket Server ตาม url ที่กำหนด
-var connection = new WebSocket('ws://'+location.host+':4000')
+var server_cube = [0,0,0];
 
-var server_cube = [0,0];
 
-connection.onopen = function () {
-  // จะทำงานเมื่อเชื่อมต่อสำเร็จ
-  console.log("connect webSocket");
-  connection.send("Hello ABCDEF"); // ส่ง Data ไปที่ Server
-};
-connection.onerror = function (error) {
-  console.error('WebSocket Error ' + error);
-};
-connection.onmessage = function (e) {
-  // log ค่าที่ถูกส่งมาจาก server
-  console.log("message from server: ", e.data);
-  server_cube = JSON.parse(e.data)['ultra'];
-};
+function makeConnection(info) {
+
+	var connection = new WebSocket('ws://'+info.address+':'+info.port)
+
+	connection.onopen = function () {
+	  // จะทำงานเมื่อเชื่อมต่อสำเร็จ
+	  console.log("connect webSocket");
+	  connection.send("Hello ABCDEF"); // ส่ง Data ไปที่ Server
+	};
+	connection.onerror = function (error) {
+	  console.error('WebSocket Error ' + error);
+	};
+	connection.onmessage = function (e) {
+	  // log ค่าที่ถูกส่งมาจาก server
+	  console.log("message from server: ", e.data);
+	  server_cube = JSON.parse(e.data)['ultra'];
+	};
+
+}
+
+fetch('/ws_info').then(response => response.json()).then(data => makeConnection(data));
